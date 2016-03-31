889b1ae58c7b316df9af9241d8b03ac37154540f
kblny <ktgr45@hotmail.com>
2016-03-31 17:24:31 +0200

    Examples projects added

---
+++ index.android.js
@@ -0,0 +1,12 @@
+/**
+ * Name: MessageBar Test Project
+ * Description: A Project to test the Message Bar Component
+ * https://github.com/KBLNY/react-native-message-bar
+ */
+'use strict';
+
+const React = require('react-native');
+const { AppRegistry, } = React;
+const AppTest = require('./AppTest');
+
+AppRegistry.registerComponent('MessageBar', () => AppTest);
